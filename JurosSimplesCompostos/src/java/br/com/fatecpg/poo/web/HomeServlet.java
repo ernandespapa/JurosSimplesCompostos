3388dfc0a3fae4f421195eb27b4177bd8dc230ba
T-Gamer <T-Gamer@DESKTOP-U26BJ62>
2019-08-17 03:58:49 -0300

    Calculo do juros composto e referencia para pagina do composto

--- JurosSimplesCompostos/src/java/br/com/fatecpg/poo/web/HomeServlet.java
+++ JurosSimplesCompostos/src/java/br/com/fatecpg/poo/web/HomeServlet.java
@@ -69,7 +69,7 @@ public class HomeServlet extends HttpServlet {
             out.println("</a>");
             out.println(" </li>");
             out.println("<li class='nav-item'>");
-            out.println("<a class='nav-link' href='#'>Juros Compostos </a>");
+            out.println("<a class='nav-link' href='juros-composto.html'>Juros Compostos </a>");
             out.println("</li>");
            /*out.println("<li class='nav-item'>");
             out.println("<a class='nav-link' href='#'>Services</a>");
